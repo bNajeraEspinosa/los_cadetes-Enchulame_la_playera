34962d72fd95f44f47a35c537b51726b1627f6d7
Fernanda Zamudio <anaferzamudio@gmail.com>
2022-10-25 15:00:21 -0600

    corrección de funcionamiento de submit

--- contactanos.js
+++ contactanos.js
@@ -3,35 +3,31 @@
     'use strict'
 
     // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-    const forms = document.querySelectorAll('.needs-validation')
+    const forms = document.querySelectorAll('.needs-validation');
 
     // Bucle sobre los campos para evitar el envío
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
+            //Si la validación arroja falso no se enviará el registro
             if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
+                event.preventDefault();
+                event.stopPropagation();
+            } else { //Si la validación arroja verdadero se hará el guardado de los datos ingresados en los inputs
+                //
+                const objFormCont = document.forms["formularioCont"];
+                let nombreCont = objFormCont.elements['input-nombre-contactanos'].value;
+                let apellidoCont = objFormCont.elements['input-apellido-contactanos'].value;
+                let correoCont = objFormCont.elements['input-correo-contactanos'].value;
+                let telCont = objFormCont.elements['input-tel-contactanos'].value;
+                let mensCont = objFormCont.elements['input-mensaje-contactanos'].value;
+                // Impresión en consola para verificar
+                console.log("Nombre: " + nombreCont);
+                console.log("Apellido: " + apellidoCont);
+                console.log("Correo: " + correoCont);
+                console.log("Teléfono: " + telCont);
+                console.log("Mensaje: " + mensCont);
             }
-            form.classList.add('was-validated')
+            form.classList.add('was-validated');
         }, false)
     });
-})()
-
-// Lectura de los campos del formulario de página Contáctanos
-const objFormCont = document.forms["formularioCont"];
-// Registra el evento al dar click en botón submit
-objFormCont.addEventListener('submit', event => {
-    event.preventDefault();
-    console.log("El evento submit se ha generado");
-    let nombreCont = objFormCont.elements['input-nombre-contactanos'].value;
-    let apellidoCont = objFormCont.elements['input-apellido-contactanos'].value;
-    let correoCont = objFormCont.elements['input-correo-contactanos'].value;
-    let telCont = objFormCont.elements['input-tel-contactanos'].value;
-    let mensCont = objFormCont.elements['input-mensaje-contactanos'].value;
-    console.log("Nombre: "+ nombreCont);
-    console.log("Apellido: "+ apellidoCont);
-    console.log("Correo: "+ correoCont);
-    console.log("Teléfono: "+ telCont);
-    console.log("Mensaje: "+ mensCont);
-    objFormCont.reset();
-});
+})();
