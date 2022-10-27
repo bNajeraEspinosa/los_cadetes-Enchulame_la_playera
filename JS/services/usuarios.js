8ff45a4464da7e14740f57298a6dbf83ad5102cc
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-10-27 18:30:43 -0500

    Remover console log

--- JS/services/usuarios.js
+++ JS/services/usuarios.js
@@ -1,7 +1,6 @@
 import fetch from '../utils/fetch.js'
 
 export const postUser = async () => {
-  console.log('JITOMATE!');
   try {
     return await fetch.post({
       url: 'http://localhost:3000/productos'
