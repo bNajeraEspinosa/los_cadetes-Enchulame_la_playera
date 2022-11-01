207f8107b3ca96c2334014df09b0755561de7259
bNajeraEspinosa <bnajeraespinosa@icloud.com>
2022-11-01 10:24:12 -0600

    botones

--- productos experimentos/app.js
+++ productos experimentos/app.js
@@ -1,15 +1,39 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+const items= document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragmento = document.createDocumentFragment();
+
+///////////////////////////////////////////////////////////
+//Aqui ejecutamos nuestra función fetchData
+document.addEventListener('DOMContentLoaded', (event) => {
     fetchData();
 });
 
+//generamos función fetchData
 const fetchData = async () => {
     try {
         const res = await fetch('api.json')
         const data = await res.json()
-        console.log(data)
+       /*  console.log(data) */
+        cartas(data)
     } catch (error) {
         console.log(error)
         
     }
+
+}
+////////////////////////////////////////////////////////
+
+const cartas = data => {
+    data.forEach(producto => {
+        templateCard.getElementById('titulo').textContent = producto.name;
+        templateCard.getElementById('descripción').textContent = producto.description;
+        templateCard.getElementById('imagen').textContent = producto.img;
+
+
+        const clonar = templateCard.cloneNode(true);
+        fragmento.appendChild(clonar);
+    })
+
+    items.appendChild(fragmento);
 
 }
