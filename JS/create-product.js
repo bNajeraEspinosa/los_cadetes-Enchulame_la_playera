f57f3ce33b22e84e3d180e78d3e6201ee2f6d3c0
Fernanda Zamudio <anaferzamudio@gmail.com>
2022-11-02 14:00:45 -0700

    ci: validación de formulario

--- JS/create-product.js
+++ JS/create-product.js
@@ -1,4 +1,4 @@
-const formulario=document.forms["form-create-products"];
+const formulario=document.getElementById("form-create-products");
 
 console.log(formulario);
 
@@ -6,6 +6,12 @@ const crearProducto = (event) => {
     //Se evita la carga de la página
     event.preventDefault();
 
-    
+    // Validación del formulario
+    const isValid = formulario.checkValidity();
+    console.log(isValid);
+    if (!isValid) return formulario.classList.add('was-validated');
+    formulario.classList.remove('was-validated');
 
 }
+
+formulario.addEventListener("submit", crearProducto);
