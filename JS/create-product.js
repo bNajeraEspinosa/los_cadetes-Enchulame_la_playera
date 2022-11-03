db4a9d991bdd6d2d935aba328d37e6f159b9b281
Fernanda Zamudio <anaferzamudio@gmail.com>
2022-11-03 10:15:13 -0700

    feat: función remover HTML

--- JS/create-product.js
+++ JS/create-product.js
@@ -4,23 +4,30 @@ const formulario = document.getElementById("form-create-products");
 const toastLiveExample = document.getElementById('liveToast');
 
 //Función para crear los elementos de la alerta de acuerdo al mensaje
-const alerta = (encabezado,cuerpo,tipoEncab,svg) => {
-    const contAlerta = document.createElement('div')
-    contAlerta.innerHTML = [
-        `<div class="toast-header text-${tipoEncab}">`,
+const alerta = (encabezado, cuerpo, tipoEncab, svg) => {
+    const contAlerta = document.createElement('div') //creación del div
+    contAlerta.innerHTML = [ //HTML del nuevo div
+        `<div class="toast-header text-${tipoEncab}" id="encabToast">`,
         `<img src="${svg}" class="rounded me-2">`,
         `<strong class="me-auto">${encabezado}</strong>`,
         '<button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>',
         '</div>',
-        `<div class="toast-body text-dark">${cuerpo}</div>`
-    ].join('')
-    toastLiveExample.append(contAlerta)
+        `<div class="toast-body text-dark" id="cuerpoToast">${cuerpo}</div>`
+    ].join('') //Unión de los elementos del arreglo en una cadena
+    toastLiveExample.append(contAlerta) //Se agrega nuevo div dentro de otro div
 
-    const toast = new bootstrap.Toast(toastLiveExample)
+    const toast = new bootstrap.Toast(toastLiveExample) //Se instancía toast
 
-    toast.show()
+    toast.show() //Se muestra toast
 }
 
+//Función para eliminar los elementos HTML del toast y evitar acumulación
+const remover = () => {
+    const encabezadoA = document.getElementById('encabToast');
+    const cuerpoA = document.getElementById('cuerpoToast');
+    encabezadoA.remove();
+    cuerpoA.remove();
+}
 
 //Función async para validar formulario y fetch
 const crearProducto = async (event) => {
@@ -34,7 +41,7 @@ const crearProducto = async (event) => {
 
     //Conversión de las entradas del formulario a objeto con atributos
     const data = Object.fromEntries(new FormData(formulario));
-    
+
     //Fetch con try/catch
     try { //Tratará guardar en response después de esperar el fetch
         let response = await fetch("https://reqres.in/api/users", {
@@ -42,9 +49,11 @@ const crearProducto = async (event) => {
             body: JSON.stringify(data) //Valores de formulario convertido a JSON
         });
         let json = await response.json() //Espera respuesta, convierte json a objeto
-        alerta("REGISTRO EXITOSO","¡Has registrado exitosamente el producto!","success","/assets/icons/check-circle-fill.svg"); //Alerta de registro exitoso
+        alerta("REGISTRO EXITOSO", "¡Has registrado exitosamente el producto!", "success", "/assets/icons/check-circle-fill.svg"); //Alerta de registro exitoso
+        setTimeout(remover, 6000);//Función con delay para eliminar HTML de la alerta y evitar acumulación
     } catch (err) { //catch en caso de error
-        alerta("¡HUBO UN ERROR!",`Error al registrar producto. ${err}`,"danger","/assets/icons/x-circle-fill.svg"); //Alerta de error
+        alerta("¡HUBO UN ERROR!", `Error al registrar producto. ${err}`, "danger", "/assets/icons/x-circle-fill.svg"); //Alerta de error
+        setTimeout(remover, 6000);
     }
     //Limpieza del formulario
     formulario.reset();
