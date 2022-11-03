fa2a9450e6d57987c3d846922964d12ffc9333d7
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-11-03 16:21:13 -0600

    refactor: envio del formulario de contatanos

--- JS/contactanos.js
+++ JS/contactanos.js
@@ -1,33 +1,22 @@
-// JavaScript de inicio para deshabilitar el envío de formularios si hay campos no válidos
-(() => {
-    'use strict'
+//Se leen todos los elementos del formulario
+const formulario = document.getElementById("formularioCont");
 
-    // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-    const forms = document.querySelectorAll('.needs-validation');
+//Función async para validar formulario y fetch
+export const submitContactanos = async (event) => {
+    //Se evita la carga de la página
+    event.preventDefault();
+    const formulario = event.currentTarget;
 
-    // Bucle sobre los campos para evitar el envío
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            //Si la validación arroja falso no se enviará el registro
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            } else { //Si la validación arroja verdadero se hará el guardado de los datos ingresados en los inputs
-                //
-                const objFormCont = document.forms["formularioCont"];
-                let nombreCont = objFormCont.elements['input-nombre-contactanos'].value;
-                let apellidoCont = objFormCont.elements['input-apellido-contactanos'].value;
-                let correoCont = objFormCont.elements['input-correo-contactanos'].value;
-                let telCont = objFormCont.elements['input-tel-contactanos'].value;
-                let mensCont = objFormCont.elements['input-mensaje-contactanos'].value;
-                // Impresión en consola para verificar
-                console.log("Nombre: " + nombreCont);
-                console.log("Apellido: " + apellidoCont);
-                console.log("Correo: " + correoCont);
-                console.log("Teléfono: " + telCont);
-                console.log("Mensaje: " + mensCont);
-            }
-            form.classList.add('was-validated');
-        }, false)
-    });
-})();
+    // Validación del formulario
+    const isValid = formulario.checkValidity();
+    if (!isValid) return formulario.classList.add('was-validated');
+    formulario.classList.remove('was-validated');
+
+    //Conversión de las entradas del formulario a objeto con atributos
+    const data = Object.fromEntries(new FormData(formulario));
+
+
+    formulario.reset();
+}
+
+formulario?.addEventListener("submit", submitContactanos);
