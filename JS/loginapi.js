54839d16c49361a7bc1e64cabcf31149950a4b55
Luisito <112416841+Luisindev12@users.noreply.github.com>
2022-11-03 19:57:04 -0600

    change function

--- JS/loginapi.js
+++ JS/loginapi.js
@@ -7,31 +7,65 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail());
   } */
 
-(() => {
-    'use strict'
-    // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-    const forms = document.querySelectorAll('.needs-validation');
-    // Bucle sobre los campos para evitar el envío
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            //Si la validación arroja falso no se enviará el registro
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            } else { //Si la validación arroja verdadero se hará el guardado de los datos ingresados en los inputs
-                const objFormCont = document.forms["login-form"];
-                let correo = objFormCont.elements['email'].value;
-                let contraseña = objFormCont.elements['password'].value;
+//Se leen todos los elementos del formulario
+const formulario = document.getElementById("login-form");
+//Se lee el elemento para la alerta de producto registrado
+const toastLiveExample = document.getElementById('liveToast');
 
-                // Impresión en consola para verificar
-                console.log("Email: " + correo);
-                console.log("Contraseña: " + contraseña);
+//Función para crear los elementos de la alerta de acuerdo al mensaje
+const alerta = (encabezado, cuerpo, tipoEncab, svg) => {
+    const contAlerta = document.createElement('div') //creación del div
+    contAlerta.innerHTML = [ //HTML del nuevo div
+        `<div class="toast-header text-${tipoEncab}" id="encabToast">`,
+        `<img src="${svg}" class="rounded me-2">`,
+        `<strong class="me-auto">${encabezado}</strong>`,
+        '<button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>',
+        '</div>',
+        `<div class="toast-body text-dark" id="cuerpoToast">${cuerpo}</div>`
+    ].join('') //Unión de los elementos del arreglo en una cadena
+    toastLiveExample?.append(contAlerta) //Se agrega nuevo div dentro de otro div
 
-                localStorage.set("login-form",JSON.stringify(correo, contraseña));
-                var credentials = JSON.parse(localStorage.get("items"));
-                console.log(credentials.inputs);
-            }
-            form.classList.add('was-validated');
-        }, false)
-    });
-})();
+    const toast = new bootstrap.Toast(toastLiveExample) //Se instancía toast
+
+    toast.show() //Se muestra toast
+}
+
+//Función para eliminar los elementos HTML del toast y evitar acumulación
+const remover = () => {
+    const encabezadoA = document.getElementById('encabToast');
+    const cuerpoA = document.getElementById('cuerpoToast');
+    encabezadoA.remove();
+    cuerpoA.remove();
+}
+
+//Función async para validar formulario y fetch
+export const crearProducto = async (event) => {
+    //Se evita la carga de la página
+    event.preventDefault();
+    const formulario = event.currentTarget;
+
+    // Validación del formulario
+    const isValid = formulario.checkValidity();
+    if (!isValid) return formulario.classList.add('was-validated');
+    formulario.classList.remove('was-validated');
+
+    //Conversión de las entradas del formulario a objeto con atributos
+    const data = Object.fromEntries(new FormData(formulario));
+
+    //Fetch con try/catch
+    try { //Tratará guardar en response después de esperar el fetch
+        let response = await fetch("https://reqres.in/api/users", {
+            method: "POST", //Se configura el método POST
+            body: JSON.stringify(data) //Valores de formulario convertido a JSON
+        });
+        let json = await response.json() //Espera respuesta, convierte json a objeto
+        alerta("REGISTRO EXITOSO", "¡Te has registrado correctamente!", "success", "/assets/icons/check-circle-fill.svg"); //Alerta de registro exitoso
+    } catch (err) { //catch en caso de error
+        alerta("¡HUBO UN ERROR!", `Error al registrar producto. ${err}`, "danger", "/assets/icons/x-circle-fill.svg"); //Alerta de error
+        setTimeout(remover, 6000);
+    }
+    //Limpieza del formulario
+    formulario.reset();
+}
+
+formulario?.addEventListener("submit", crearProducto);
