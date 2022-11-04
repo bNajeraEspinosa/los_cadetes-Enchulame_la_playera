de5e653225c3f9806dc41d0d95ab2ca6cca1d9ba
Luisito <112416841+Luisindev12@users.noreply.github.com>
2022-11-03 21:42:40 -0600

    ya me quiero ir a dormir.JSON

--- JS/loginapi.js
+++ JS/loginapi.js
@@ -1,32 +1,25 @@
-//Se leen todos los elementos del formulario
+
 const formulario = document.getElementById("login-form");
 
-//Función async para validar formulario y fetch
-const añadirUsuario = async (event) => {
-    //Se evita la carga de la página
+const login = async (event) => {
+
     event.preventDefault();
     const formulario = event.currentTarget;
-
-    // Validación del formulario
     const isValid = formulario.checkValidity();
+
     if (!isValid) return formulario.classList.add('was-validated');
     formulario.classList.remove('was-validated');
 
-    //Conversión de las entradas del formulario a objeto con atributos
-    const data = Object.fromEntries(new FormData(formulario));
 
-    //Fetch con try/catch
-    try { //Tratará guardar en response después de esperar el fetch
-        let response = await fetch("https://reqres.in/api/users", {
-            method: 'POST', //Se configura el método POST
-            body: JSON.stringify(data) //Valores de formulario convertido a JSON
-        })
-    } catch (err) {
-        console.log("error")
-    }
-    //Limpieza del formulario
-    formulario.reset();
+     const data = Object.fromEntries(new FormData(formulario));
 
+     const response = await fetch("https://reqres.in/api/users", {
+        body: JSON.stringify(data),
+        method: 'POST'
+    })
+    const userData = await response.json();
+    console.log(userData)
+    localStorage.setItem("USUARIO", JSON.stringify(data));
 
-    formulario?.addEventListener("submit", añadirUsuario);
 }
+    formulario?.addEventListener('submit', login);
