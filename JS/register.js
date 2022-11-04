e8d1e26e7abb44123f4c0ea33d8d6b145950fe11
Carolina Peraza <carolina.peranie@gmail.com>
2022-11-03 22:32:51 -0600

    Se agrega redireccionamiento y localStorage

--- JS/register.js
+++ JS/register.js
@@ -1,23 +1,33 @@
-
-
-
 const formElement = document.getElementById("register-form");
 
 const submitRegister = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const isValid = formElement.checkValidity();
-    if (!isValid) return formElement.classList.add('was-validated');
-    formElement.classList.remove('was-validated');
+  const isValid = formElement.checkValidity();
+  if (!isValid) return formElement.classList.add("was-validated");
+  formElement.classList.remove("was-validated");
 
-    const data = Object.fromEntries(new FormData(formElement));
+  const data = Object.fromEntries(new FormData(formElement));
 
+  const response = await fetch("https://reqres.in/api/users", {
+    body: JSON.stringify(data),
+    method: "POST",
+  });
+
+  const userData = await response.json();
+
+  try {
     const response = await fetch("https://reqres.in/api/users", {
-        body: JSON.stringify(data),
-        method:'POST'
-    })
-    
+      body: JSON.stringify(data),
+      method: "POST",
+    });
     const userData = await response.json();
-}
+  } catch (error) {
+    console.log(error);
+  }
 
-formElement.addEventListener('submit', submitRegister);
+  localStorage.setItem("cur_user", JSON.stringify(data));
+  //console.log(userData);
+  window.location.href = "/index.html";
+};
+formElement.addEventListener("submit", submitRegister);
