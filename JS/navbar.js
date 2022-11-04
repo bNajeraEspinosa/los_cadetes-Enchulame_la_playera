41049878717db9557591a358745433223b44f7d8
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-11-04 03:55:58 -0600

    Revert "feat: paginas de registro, login y creacion de produtos (#32)"
    This reverts commit bfe391f60057408d8f5b34cb81438f07cce30cce.

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,38 +1,34 @@
-import { NAV_LINKS, PROFILE } from './constants/nav-data.js';
+import { navLinks, excludeLinks, pathIcons } from './constants/index.js';
+
 
 const generateNavLink = (href, label) => `
   <li class="tab">
-    <a class="nav-link fs-6 rounded" href="${href}" >
+    <a
+      class="nav-link fs-6 rounded"
+      href="${href}"
+    >
       ${label}
     </a>
-  </li>
-`
-export const generateNavBtnIcon = (label, icon, url) => `
-  <a class="d-flex gap-2 align-items-center" style="cursor: pointer; text-decoration:none;" href="${url}">
-    <img class="w-100" style="filter: invert(1)" src="${icon}" alt="${label}-icon" />
-    <p class="fw-semibold text-white fs-6 m-0">${label}</p>
-  </a>
-  `
-export const generateNavDropdownBtn = (label, path, list) => `
-  <div class="btn-group">
-    <button class="btn btn-sm d-flex gap-2 align-items-center rounded dropdown-menu-end" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      <img class="w-100" style="filter: invert(1)" src="${path}" alt="${label}-icon" />
-      <p class="fw-semibold text-white fs-6 m-0">${label}</p>
-    </button>
-    <ul class="dropdown-menu dropdown-menu-lg-end dropdown-menu-dark border border-2 border-dark">
-      ${list}
-    </ul>
-  </div>
-`
+  </li>`
 
-const loadNavbar = (user) => {
+const generateNavBtnIcon = (label, path, url) => `
+    <a class="d-flex gap-2" style="cursor: pointer; text-decoration:none;" href="${url}">
+      <img class="w-100" style="filter: invert(1)" src="${path}" alt="${label}-icon" />
+      <p class="fw-semibold text-white f-6 m-0">${label}</p>
+    </a>
+  `
+
+const loadNavbar = () => {
+  if (window.location.pathname === '/index.html') window.location.href = '/'
+  const currentSite = navLinks.find((navLink) => window.location.pathname.toLowerCase() === navLink.href)
+
   const navbarHTML = `
-    <div class="navbar nav navbar-expand-lg navbar-light justify-content-center p-0" >
-      <div class="container-fluid py-2 w-90">
+    <div class="navbar nav ${currentSite.style} navbar-expand-lg navbar-light justify-content-center p-0" >
+      <div class="container-fluid py-2">
         <a class="navbar-brand" href="/">
           <img
             src="https://i.postimg.cc/TwKBhCqZ/logo-basico.png"
-            alt="logo"
+            alt=""
             width="80"
             height="40"
             class="img-fluid"
@@ -42,27 +38,40 @@ const loadNavbar = (user) => {
 
     <!-- BOTÓN DE RESPONSIVE -->
     <button
-      class="navbar-toggler border-white border-opacity-25 d-flex d-lg-none px-2"
+      class="navbar-toggler border-white border-opacity-25"
       type="button"
-      style="transform: scale(1.25)"
       data-bs-toggle="collapse"
       data-bs-target="#navbarSupportedContent"
       aria-controls="navbarSupportedContent"
       aria-expanded="false"
       aria-label="Toggle navigation"
     >
-      <img src="/assets/icons/burger.svg" class="fs-1 filter-invert" />
+      <svg
+        width="32"
+        height="32"
+        fill="#fff"
+        class="bi bi-list"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
     </button>
 
     <!-- LINKS DE LA NAV -->
     <div class="collapse navbar-collapse justify-content-end gap-4" id="navbarSupportedContent">
-      <ul class="navbar-nav nav text-center pb-lg-0">
-        ${Object.values(NAV_LINKS).map(({ path, label }) => generateNavLink(path, label)).join('')}
+      <ul class="navbar-nav nav text-center pb-3 pb-lg-0">
+      ${navLinks
+      .filter(link => !excludeLinks.includes(link.href))
+      .map(link => generateNavLink(link.href, link.label))
+      .join('')}
       </ul>
 
       <!-- ICONOS DE CARRITO Y LOGIN -->
-        <div class="d-flex justify-content-center gap-3 py-2 align-items-center" >
-        ${generateNavDropdownBtn(user ? user.username : PROFILE.label, PROFILE.icon, user ? PROFILE.list.online.join('') : PROFILE.list.offline.join(''))}
+        <div class="d-flex justify-content-center gap-3" >
+          ${pathIcons.map(icon => generateNavBtnIcon(icon.label, icon.path,icon.url)).join('')}
         </div>
       </div>
     </div>
@@ -72,20 +81,4 @@ const loadNavbar = (user) => {
   document.getElementById('navbar').innerHTML = navbarHTML;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const user = JSON.parse(localStorage.getItem('cur_user'));
-  const offlinePages = ['/html/login.html', '/html/register.html'];
-  const currentPage = window.location.pathname;
-  if (offlinePages.includes(currentPage)) location.replace('/index.html');
-  loadNavbar(user);
-
-
-  if (user) {
-    const btnLogoutEl = document.getElementById('btn-logout');
-    btnLogoutEl?.addEventListener('click', () => {
-      localStorage.removeItem('cur_user');
-      window.location.reload();
-    });
-  }
-
-});
+loadNavbar()
