bb31157116b39769b8758b0b6f26014b0003dea8
bNajeraEspinosa <bnajeraespinosa@icloud.com>
2022-11-03 19:15:06 -0600

    arreglos

--- JS/productos.js
+++ JS/productos.js
@@ -1,115 +1,42 @@
-function addItem(item) {
-    const itemHTML = 
-    `
-    <div class="col d-flex flex-column">
-        <div class="flex flex-grow-1">
-            <div>
-                <p class="fw-bold fs-3 m-0 my-1">${item.name}</p>
-                <span class="fw-bold fs-6 d-block text-uppercase">${item.by}</span>
-            </div>
-            <img class="w-50 my-3 img-fluid" src="${item.img}" >
-        </div>
-        <div>
-            <p class="fw-bold fs-3 m-0 my-1">${item.price}</p>
-            <a href="${item.url}" class="btn my-3 btn-outline-secondary "> ✧ ¿Deseas ver mas detalles? ✦ </a>
-        </div>
-    </div>
-    `
+const items = document.getElementById("items");
+const templateCard = document.getElementById("template-card").content;
+const fragmento = document.createDocumentFragment();
 
+///////////////////////////////////////////////////////////
+//Aqui ejecutamos nuestra función fetchData
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetchData();
+});
 
-    const itemsContainer = document.getElementById("hoodieProducto");
-    itemsContainer.innerHTML += itemHTML;
+//generamos función fetchData
+const fetchData = async () => {
+    try {
+        const res = await fetch('../api.json')
+        const data = await res.json()
+        /*  console.log(data) */
+        cartas(data)
+    } catch (error) {
+        console.log(error)
+
+    }
+
 }
+////////////////////////////////////////////////////////
 
-addItem({
-    name: "MANGA",
-    img: "../assets/img/MANGA.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Hoodie, con diseño anime/manga"
-});
+const cartas = data => {
+    data.forEach(producto => {
 
-addItem({
-    name: "COMICS",
-    img: "../assets/img/COMICS.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Hoodie, con diseño comic/superhéroe"
-});
+        /* templateCard.getElementById("identificación").textContent = producto.id; */
+        templateCard.getElementById("name").textContent = producto.name;
+        templateCard.getElementById("imagen").setAttribute("src", producto.imagen);
+        /* templateCard.getElementById("url").setAttribute("href", producto.url);  no se como agregar esto desde sus documentos */
+        templateCard.getElementById("price").textContent = producto.price;
+        templateCard.getElementById("by").textContent = producto.by;
+        /*  */
+        const clonar = templateCard.cloneNode(true);
+        fragmento.appendChild(clonar);
+    })
+    /* console.log(fragmento); */
+    items.appendChild(fragmento);
 
-addItem({
-    name: "TV SHOW",
-    img: "../assets/img/TV SHOW.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Playera, con diseño cartoon/avatar"
-});
-
-addItem({
-    name: "TV SHOW",
-    img: "../assets/img/APPA.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Hoodie, con diseño TV SERIE/Saul G."
-});
-
-addItem({
-    name: "TV SHOW",
-    img: "../assets/img/AVATAR.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Playera, con diseño cartoon/avatar"
-});
-
-addItem({
-    name: "FAMOUS",
-    img: "../assets/img/FAMOUS.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Hoodie, con diseño Película/4th Dimention"
-});
-
-addItem({
-    name: "FAMOUS",
-    img: "../assets/img/BRUNO.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Playera, con Treasure... digo Bruno Mars"
-});
-
-addItem({
-    name: "FAMOUS",
-    img: "../assets/img/BTS.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Playera, con IDOLS... lol"
-});
-
-addItem({
-    name: "RETRO",
-    img: "../assets/img/RETRO.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by: "Playera, con frase letras retro"
-});
-
-addItem({
-    name: "FRASES",
-    img: "../assets/img/FRASE.png",
-    description: "Orange and Apple juice fresh and delicious",
-    url: "/html/producto-info.html",
-    price:'$300 mxn',
-    by:"Playera, con frase graciosa/cool"
-});
-
-
-
+}
