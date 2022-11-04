75b029b9baea944a928500745940a82dbae0edd7
ELiud Sosa <eliud.sosa97@gmail.com>
2022-11-03 21:32:02 -0600

    Register exitoso

--- JS/register.js
+++ JS/register.js
@@ -7,14 +7,17 @@ const submitRegister = async (e) => {
     e.preventDefault();
 
     const isValid = formElement.checkValidity();
-    if (!isValid) return formElement.classList.remove('was-validated');
+    if (!isValid) return formElement.classList.add('was-validated');
+    formElement.classList.remove('was-validated');
 
     const data = Object.fromEntries(new FormData(formElement));
 
-    const userData = await fetch("https://reqres.in/api/users", {
-        body, method:'POST' .JSON.stringify(data),
+    const response = await fetch("https://reqres.in/api/users", {
+        body: JSON.stringify(data),
+        method:'POST'
     })
-    console.log(userData);
+    
+    const userData = await response.json();
 }
 
 formElement.addEventListener('submit', submitRegister);
