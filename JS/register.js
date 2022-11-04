646e69d27133848f42f244c0abc536978c4e9e8c
ELiud Sosa <eliud.sosa97@gmail.com>
2022-11-03 20:43:34 -0600

    Acomodando

--- JS/register.js
+++ JS/register.js
@@ -1,20 +1,20 @@
 
 
 
-const formElement = document.getElementById("login-form");
+const formElement = document.getElementById("register-form");
 
 const submitRegister = async (e) => {
     e.preventDefault();
 
-    const isValid = formElement.checkVAlidity();
+    const isValid = formElement.checkValidity();
     if (!isValid) return formElement.classList.remove('was-validated');
 
     const data = Object.fromEntries(new FormData(formElement));
 
     const userData = await fetch("https://reqres.in/api/users", {
-        body: JSON.stringify(data),
+        body, method:'POST' .JSON.stringify(data),
     })
-    setLocalUser(userData);
+    console.log(userData);
 }
 
 formElement.addEventListener('submit', submitRegister);
