b9e361ede12acee438a149db82e7511a94b30d51
Carolina Peraza <carolina.peranie@gmail.com>
2022-11-03 23:07:02 -0600

    Se agrega TRY y CATCH y  redireccion a home

--- JS/loginapi.js
+++ JS/loginapi.js
@@ -1,25 +1,34 @@
-
 const formulario = document.getElementById("login-form");
 
 const login = async (event) => {
+  event.preventDefault();
+  const formulario = event.currentTarget;
+  const isValid = formulario.checkValidity();
 
-    event.preventDefault();
-    const formulario = event.currentTarget;
-    const isValid = formulario.checkValidity();
+  if (!isValid) return formulario.classList.add("was-validated");
+  formulario.classList.remove("was-validated");
 
-    if (!isValid) return formulario.classList.add('was-validated');
-    formulario.classList.remove('was-validated');
+  const data = Object.fromEntries(new FormData(formulario));
 
+  const response = await fetch("https://reqres.in/api/users", {
+    body: JSON.stringify(data),
+    method: "POST",
+  });
+  const userData = await response.json();
 
-     const data = Object.fromEntries(new FormData(formulario));
-
-     const response = await fetch("https://reqres.in/api/users", {
-        body: JSON.stringify(data),
-        method: 'POST'
-    })
+  try {
+    const response = await fetch("https://reqres.in/api/users", {
+      body: JSON.stringify(data),
+      method: "POST",
+    });
     const userData = await response.json();
-    console.log(userData)
-    localStorage.setItem("USUARIO", JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
 
-}
-    formulario?.addEventListener('submit', login);
+  localStorage.setItem("cur_user", JSON.stringify(userData));
+
+  window.location.href = "/index.html";
+};
+
+formulario?.addEventListener("submit", login);
