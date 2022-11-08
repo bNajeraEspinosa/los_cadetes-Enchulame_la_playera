421fd16f1f901b3d3d4fcd7d1c201a5ea97ec873
Fernanda Zamudio <113369206+anafzo@users.noreply.github.com>
2022-11-07 18:54:25 -0700

    style: formateo con prettier a todos los archivos (#40)

--- JS/productos.js
+++ JS/productos.js
@@ -4,38 +4,38 @@ const fragmento = document.createDocumentFragment();
 
 ///////////////////////////////////////////////////////////
 //Aqui ejecutamos nuestra función fetchData
-document.addEventListener('DOMContentLoaded', (event) => {
-    fetchData();
+document.addEventListener("DOMContentLoaded", (event) => {
+  fetchData();
 });
 
 //generamos función fetchData
 const fetchData = async () => {
-    try {
-        const res = await fetch('https://mockend.com/alaanescobedo/db-server/products?limit=15');
-        const data = await res.json()
-        /*  console.log(data) */
-        cartas(data)
-    } catch (error) {
-        console.log(error)
-
-    }
-
-}
+  try {
+    const res = await fetch(
+      "https://mockend.com/alaanescobedo/db-server/products?limit=15"
+    );
+    const data = await res.json();
+    /*  console.log(data) */
+    cartas(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 ////////////////////////////////////////////////////////
 
-const cartas = data => {
-    data.forEach(producto => {
-
-        templateCard.getElementById("name").textContent = producto.name;
-        templateCard.getElementById("img").setAttribute("src", producto.img);
-        templateCard.getElementById("url").setAttribute("href", `/html/producto-info.html?id=${producto.id}`);
-        templateCard.getElementById("price").textContent = producto.price;
-        templateCard.getElementById("by").textContent = producto.by;
-        /*  */
-        const clonar = templateCard.cloneNode(true);
-        fragmento.appendChild(clonar);
-    })
-    /* console.log(fragmento); */
-    items.appendChild(fragmento);
-
-}
+const cartas = (data) => {
+  data.forEach((producto) => {
+    templateCard.getElementById("name").textContent = producto.name;
+    templateCard.getElementById("img").setAttribute("src", producto.img);
+    templateCard
+      .getElementById("url")
+      .setAttribute("href", `/html/producto-info.html?id=${producto.id}`);
+    templateCard.getElementById("price").textContent = producto.price;
+    templateCard.getElementById("by").textContent = producto.by;
+    /*  */
+    const clonar = templateCard.cloneNode(true);
+    fragmento.appendChild(clonar);
+  });
+  /* console.log(fragmento); */
+  items.appendChild(fragmento);
+};
--- __tests__/loginapi.test.js
+++ __tests__/loginapi.test.js
@@ -1,10 +1,10 @@
-import { jest } from '@jest/globals';
-import { login } from '../JS/loginapi.js'
+import { jest } from "@jest/globals";
+import { login } from "../JS/loginapi.js";
 
 const mockProduct = {
-  username: 'Test Item',
-  password: '123456'
-}
+  username: "Test Item",
+  password: "123456",
+};
 
 const mockForm = {
   checkValidity: jest.fn(() => true),
@@ -14,103 +14,107 @@ const mockForm = {
   },
   reset: jest.fn(),
   addEventListener: jest.fn(),
-}
+};
 
-global.FormData = jest.fn()
+global.FormData = jest.fn();
 
-const response = { json: jest.fn(() => Promise.resolve(mockProduct)) }
+const response = { json: jest.fn(() => Promise.resolve(mockProduct)) };
 
-jest.spyOn(Object, 'fromEntries').mockImplementation(() => mockProduct)
+jest.spyOn(Object, "fromEntries").mockImplementation(() => mockProduct);
 
 // spy local storage
-jest.spyOn(Storage.prototype, 'setItem')
+jest.spyOn(Storage.prototype, "setItem");
 
 // mock window.location.href
 global.window = Object.create(window);
-Object.defineProperty(window, 'location', {
+Object.defineProperty(window, "location", {
   value: {
     href: jest.fn(),
-  }
+  },
 });
 
-describe('loginapi.js', () => {
-
+describe("loginapi.js", () => {
   beforeEach(() => {
-    jest.clearAllMocks()
-    global.fetch = jest.fn().mockImplementation(() => Promise.resolve(response))
-  })
+    jest.clearAllMocks();
+    global.fetch = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(response));
+  });
 
-  describe('login function', () => {
+  describe("login function", () => {
     const mockEvent = {
       preventDefault: jest.fn(),
       currentTarget: mockForm,
-    }
+    };
     // Evento
-    it('Debe prevenir el comportamiento por defecto del formulario', async () => {
-      await login(mockEvent)
-      expect(mockEvent.preventDefault).toHaveBeenCalledTimes(1)
-    })
+    it("Debe prevenir el comportamiento por defecto del formulario", async () => {
+      await login(mockEvent);
+      expect(mockEvent.preventDefault).toHaveBeenCalledTimes(1);
+    });
     // Validaciones
-    it('Debe validar el formulario', async () => {
-      await login(mockEvent)
-      expect(mockForm.checkValidity).toHaveBeenCalled()
-    })
-    it('Debe agregar la clase was-validated al formulario si fue validado incorrectamente', async () => {
-      mockForm.checkValidity.mockReturnValueOnce(false)
-      await login(mockEvent)
-      expect(mockForm.classList.add).toHaveBeenCalledWith('was-validated')
-    })
-    it('Debe detener la ejecución del formulario si no es válido', async () => {
-      mockForm.checkValidity.mockReturnValueOnce(false)
-      await login(mockEvent)
-      expect(global.fetch).not.toHaveBeenCalled()
-    })
-    it('Debe remover la clase was-validated al formulario si fue validado correctamente', async () => {
-      await login(mockEvent)
-      expect(mockForm.classList.remove).toHaveBeenCalledWith('was-validated')
-    })
+    it("Debe validar el formulario", async () => {
+      await login(mockEvent);
+      expect(mockForm.checkValidity).toHaveBeenCalled();
+    });
+    it("Debe agregar la clase was-validated al formulario si fue validado incorrectamente", async () => {
+      mockForm.checkValidity.mockReturnValueOnce(false);
+      await login(mockEvent);
+      expect(mockForm.classList.add).toHaveBeenCalledWith("was-validated");
+    });
+    it("Debe detener la ejecución del formulario si no es válido", async () => {
+      mockForm.checkValidity.mockReturnValueOnce(false);
+      await login(mockEvent);
+      expect(global.fetch).not.toHaveBeenCalled();
+    });
+    it("Debe remover la clase was-validated al formulario si fue validado correctamente", async () => {
+      await login(mockEvent);
+      expect(mockForm.classList.remove).toHaveBeenCalledWith("was-validated");
+    });
 
     // Conversión de entradas
-    it('Debe convertir las entradas del formulario a un objeto con atributos', async () => {
-      await login(mockEvent)
-      expect(Object.fromEntries).toHaveBeenCalledWith(new FormData(mockForm))
-      expect(Object.fromEntries).toHaveBeenCalledTimes(1)
-      expect(Object.fromEntries).toHaveReturnedWith(mockProduct)
-    })
+    it("Debe convertir las entradas del formulario a un objeto con atributos", async () => {
+      await login(mockEvent);
+      expect(Object.fromEntries).toHaveBeenCalledWith(new FormData(mockForm));
+      expect(Object.fromEntries).toHaveBeenCalledTimes(1);
+      expect(Object.fromEntries).toHaveReturnedWith(mockProduct);
+    });
 
     // Fetch
-    it('Debe ejecutar el fetch con los datos del formulario', async () => {
-      await login(mockEvent)
-      expect(global.fetch).toHaveBeenCalledWith('https://mockend.com/alaanescobedo/db-server/users', {
-        method: 'POST',
-        body: JSON.stringify(mockProduct),
-      })
-      expect(global.fetch).toHaveBeenCalledTimes(1)
-    })
-    it('Debe llamar al metodo .json', async () => {
-      await login(mockEvent)
-      expect(response.json).toHaveBeenCalledTimes(1)
-      expect(response.json).toHaveReturnedWith(Promise.resolve(mockProduct))
-    })
+    it("Debe ejecutar el fetch con los datos del formulario", async () => {
+      await login(mockEvent);
+      expect(global.fetch).toHaveBeenCalledWith(
+        "https://mockend.com/alaanescobedo/db-server/users",
+        {
+          method: "POST",
+          body: JSON.stringify(mockProduct),
+        }
+      );
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+    });
+    it("Debe llamar al metodo .json", async () => {
+      await login(mockEvent);
+      expect(response.json).toHaveBeenCalledTimes(1);
+      expect(response.json).toHaveReturnedWith(Promise.resolve(mockProduct));
+    });
 
     // LocalStorage
-    it('Debe guardar el usuario en el localStorage', async () => {
-      await login(mockEvent)
-      expect(global.localStorage.setItem).toHaveBeenCalledWith('cur_user', JSON.stringify(mockProduct))
-      expect(global.localStorage.setItem).toHaveBeenCalledTimes(1)
-    })
+    it("Debe guardar el usuario en el localStorage", async () => {
+      await login(mockEvent);
+      expect(global.localStorage.setItem).toHaveBeenCalledWith(
+        "cur_user",
+        JSON.stringify(mockProduct)
+      );
+      expect(global.localStorage.setItem).toHaveBeenCalledTimes(1);
+    });
 
-    it('Debe limpiar el formulario', async () => {
-      await login(mockEvent)
-      expect(mockForm.reset).toHaveBeenCalled()
-    })
+    it("Debe limpiar el formulario", async () => {
+      await login(mockEvent);
+      expect(mockForm.reset).toHaveBeenCalled();
+    });
 
-    it('Debe redireccionar a la página de inicio', async () => {
-      await login(mockEvent)
-      expect(window.location.href).toBe('/index.html')
-    })
-
-  })
-
-})
-
+    it("Debe redireccionar a la página de inicio", async () => {
+      await login(mockEvent);
+      expect(window.location.href).toBe("/index.html");
+    });
+  });
+});
