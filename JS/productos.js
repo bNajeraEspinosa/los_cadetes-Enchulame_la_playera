0538156092df13ad864a6f1815ae7d465c87bfe4
Fernanda Zamudio <113369206+anafzo@users.noreply.github.com>
2022-11-09 11:33:33 -0700

    feat: placeholders productos (#42)
    * feat: img placeholder
    
    * feat: html placeholders productos
    
    * ci: comments en placeholders
    
    * ci: cambio elemento by por descrip corta
    
    * ci: declaración y llamada remover placeholders
    
    * ci: corrección acomodo placeholders
    
    * ci: corrección función remover placeholders
    
    * ci: limpieza de codigo js

--- JS/productos.js
+++ JS/productos.js
@@ -2,40 +2,41 @@ const items = document.getElementById("items");
 const templateCard = document.getElementById("template-card").content;
 const fragmento = document.createDocumentFragment();
 
-///////////////////////////////////////////////////////////
-//Aqui ejecutamos nuestra función fetchData
-document.addEventListener("DOMContentLoaded", (event) => {
-  fetchData();
-});
+//Declaración de función para eliminar elementos del HTML
+const remover = () => {
+    const objPlaceholder = document.getElementById('items');
+    objPlaceholder.replaceChildren();
+}
 
-//generamos función fetchData
+//Declaración función mostrar cards con artículos
+const cartas = data => {
+    data.forEach(producto => {
+        templateCard.getElementById("name").textContent = producto.name;
+        templateCard.getElementById("img").setAttribute("src", producto.img);
+        templateCard.getElementById("url").setAttribute("href", `/html/producto-info.html?id=${producto.id}`);
+        templateCard.getElementById("price").textContent = producto.price;
+        templateCard.getElementById("description_short").textContent = producto.description_short;
+        /*  */
+        const clonar = templateCard.cloneNode(true);
+        fragmento.appendChild(clonar);
+    })
+    /* console.log(fragmento); */
+    items.appendChild(fragmento);
+}
+
+//Declaración función async fetch
 const fetchData = async () => {
-  try {
-    const res = await fetch(
-      "https://mockend.com/alaanescobedo/db-server/products?limit=15"
-    );
-    const data = await res.json();
-    /*  console.log(data) */
-    cartas(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
-////////////////////////////////////////////////////////
+    try {
+        const res = await fetch('https://mockend.com/alaanescobedo/db-server/products?limit=15');
+        const data = await res.json();
+        remover();
+        cartas(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-const cartas = (data) => {
-  data.forEach((producto) => {
-    templateCard.getElementById("name").textContent = producto.name;
-    templateCard.getElementById("img").setAttribute("src", producto.img);
-    templateCard
-      .getElementById("url")
-      .setAttribute("href", `/html/producto-info.html?id=${producto.id}`);
-    templateCard.getElementById("price").textContent = producto.price;
-    templateCard.getElementById("by").textContent = producto.by;
-    /*  */
-    const clonar = templateCard.cloneNode(true);
-    fragmento.appendChild(clonar);
-  });
-  /* console.log(fragmento); */
-  items.appendChild(fragmento);
-};
+//Aqui ejecutamos nuestra función fetchData
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetchData();
+});
