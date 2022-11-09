44449a47e34476b8825f445b0dfe38384bb2f135
bNajeraEspinosa <bnajeraespinosa@icloud.com>
2022-11-09 12:22:11 -0600

    pagina term y condiciones edit/footer restaurado

--- JS/footer.js
+++ JS/footer.js
@@ -14,13 +14,6 @@ export const loadFooter = () => {
         
         <a href="#" type="button" class="btn btn-outline btn-md filter-invert"> <img class="img-fluid" 
         src="/assets/icons/whatsapp.svg" alt="Icono Whats App"> </a>
-        <div class="row">
-        <div class="col-md-7">
-          <p color:"black;" >copyright 2022</p>
-        </div>
-        <nav class="col-md-5">
-          <a href="HTML/terminos.html">Términos y condiciones</a>
-        </nav>
       </div>  
     </div>
   `
