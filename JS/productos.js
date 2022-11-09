84312aa0a1b2ced3fcc4dea8a190336cf97ba825
Fernanda Zamudio <anaferzamudio@gmail.com>
2022-11-08 22:43:44 -0700

    ci: cambio elemento by por descrip corta

--- JS/productos.js
+++ JS/productos.js
@@ -1,36 +1,33 @@
 const items = document.getElementById("items");
 const templateCard = document.getElementById("template-card").content;
 const fragmento = document.createDocumentFragment();
+//const tempPlaceholder = document.getElementById("template-placeholder").content;
 
-///////////////////////////////////////////////////////////
-//Aqui ejecutamos nuestra función fetchData
-document.addEventListener('DOMContentLoaded', (event) => {
-    fetchData();
-});
-
-//generamos función fetchData
+////////////////////////////////////////////////////////
+//Declaración función async fetch
 const fetchData = async () => {
     try {
         const res = await fetch('https://mockend.com/alaanescobedo/db-server/products?limit=15');
-        const data = await res.json()
-        /*  console.log(data) */
-        cartas(data)
+        const data = await res.json();
+        cartas(data);
     } catch (error) {
-        console.log(error)
-
+        console.log(error);
     }
-
 }
-////////////////////////////////////////////////////////
 
+const remover = () => {
+    const objPlaceholder = document.getElementById('contPlaceholders');
+    objPlaceholder.remove();
+}
+
+//Declaración función mostrar cards con artículos
 const cartas = data => {
     data.forEach(producto => {
-
         templateCard.getElementById("name").textContent = producto.name;
         templateCard.getElementById("img").setAttribute("src", producto.img);
         templateCard.getElementById("url").setAttribute("href", `/html/producto-info.html?id=${producto.id}`);
         templateCard.getElementById("price").textContent = producto.price;
-        templateCard.getElementById("by").textContent = producto.by;
+        templateCard.getElementById("description_short").textContent = producto.description_short;
         /*  */
         const clonar = templateCard.cloneNode(true);
         fragmento.appendChild(clonar);
@@ -39,3 +36,9 @@ const cartas = data => {
     items.appendChild(fragmento);
 
 }
+
+///////////////////////////////////////////////////////////
+//Aqui ejecutamos nuestra función fetchData
+document.addEventListener('DOMContentLoaded', (event) => {
+    fetchData();
+});
