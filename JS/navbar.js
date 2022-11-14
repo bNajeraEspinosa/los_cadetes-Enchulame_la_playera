e5818b6bafceeb85e4bc75a0825985d38445632a
Fernanda Zamudio <anaferzamudio@gmail.com>
2022-11-14 02:44:02 -0700

    ci: cambio href de carrito

--- JS/navbar.js
+++ JS/navbar.js
@@ -65,7 +65,7 @@ const loadNavbar = (user) => {
       <!-- ICONOS DE CARRITO Y LOGIN -->
         <div class="d-flex justify-content-center gap-3 py-2 align-items-center">
         <div class="d-flex gap-2 align-items-center btn" role="button">
-        <a data-bs-toggle="modal" data-bs-target="#modal">
+        <a href="/HTML/compra.html">
           <img src="/assets/icons/${CART.icon}" class="fs-1 filter-invert" />
           <span class="fw-semibold text-white fs-6 m-0" id="carritoSpan"></span>
           </a>
