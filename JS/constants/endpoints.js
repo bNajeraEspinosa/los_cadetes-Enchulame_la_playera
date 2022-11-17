e4ce85074ed15e991b1911b9f3fecf8d0743bc1b
Fernanda Zamudio <anaferzamudio@gmail.com>
2022-11-16 23:30:24 -0700

    ci: se agrega endpoint de pago

--- JS/constants/endpoints.js
+++ JS/constants/endpoints.js
@@ -3,4 +3,5 @@ const BASE_URL = "https://mockend.com/alaanescobedo/db-server";
 export const ENDPOINTS = {
   PRODUCTS: `${BASE_URL}/products`,
   AUTH_USER: `${BASE_URL}/users`,
+  PAYMENT: `${BASE_URL}/payment`
 };
